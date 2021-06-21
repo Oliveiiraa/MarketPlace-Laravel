fbac5c9835a65833fe9f630889f038ce67cf5fb3
Gabriel Oliveira <gabriel.oliveira@ikatec.com.br>
2021-06-21 15:08:48 -0300

    First Commit

---
+++ app/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'description', 'slug'];
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        \DB::table('users')->insert([
+//            'name' => 'Admin',
+//            'email' => 'admin@admin.com',
+//            'email_verified_at' => now(),
+//            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+//            'remember_token' => Str::random(10),
+//        ]);
+
+        factory(\App\User::class, 40)->create()->each(function ($user){
+            $user->store()->save(factory(\App\Store::class)->make());
+        });
+    }
+}
